37216c665ea4413be4e538b81646ed61e03da367
Albert Mokrejs <albertmokrejs@gmail.com>
2015-12-19 10:39:24 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,4 +13,16 @@ def authenticate(username,password):
         if r[1] == password:
             return [True, r[2]]
     return [False, -1]
+    
+def updateCache(cacheID, lat, lon, Type, name, desc, stat):
+    conn = sqlite3.connect("GeoHashCache.db")
+    c = conn.cursor()
+    q = """UPDATE caches 
+    SET Latitude = '%s', 
+    Longitude = '%s',
+    Type = '%s',
+    Name = '%s',
+    Description = '%s', Status = '%s'
+    WHERE Cacheid = '%s';""" % (lat, lon, Type, name, desc, stat, cacheID)
+    c.execute(q)
 
