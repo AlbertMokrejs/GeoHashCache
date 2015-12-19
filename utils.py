03b846372a3e4b17edf3f9ffee8002510ca15fda
Albert Mokrejs <albertmokrejs@gmail.com>
2015-12-19 14:32:16 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -50,11 +50,13 @@ def validateCache(cacheID, passcode):
     return False
     
 def makeNewCache(Latitude, Longitude, Type, Name, Description, Founder):
-    cacheID = greatestCacheID()
+    cacheID = greatestCacheID() + 1
     Date = time.strftime("%d-%m-%Y)
     hashed = hashlib.sha224(str(cacheID) + Date).hexdigest()
     validID = str(int(hashed,16))[0:10]
     createCache(Latitude, Longitude, Type, Name, Description, cacheID, validID, Founder, Date)
 
-
-
+def makeComment(Parentid, Content, Author):
+    Commentid = lowestCommentID() - 1
+    Date = time.strftime("%d-%m-%Y)
+    comment(Parentid, Commentid, Content, Date, Author)
