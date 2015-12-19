54844c86e96e0042fd76f64a19c89319f2f9f6c6
Albert Mokrejs <albertmokrejs@gmail.com>
2015-12-19 15:13:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,14 @@ def moveCache(cacheID = 0):
   
 
 @app.route("/closeMoveCache/<cacheID>")
-def moveCache(cacheID = 0):
+def moveCacheB(cacheID = 0):
 	return genNewCoord(cacheID, True)
+	
+@app.route("/checkCache/<cacheID>/<validID>")
+def checkCache(cacheID = 0, validID = 0):
+	return validateCache(cacheID, validID)
+
+
 
 if (__name__ == "__main__"):
         app.debug = True
