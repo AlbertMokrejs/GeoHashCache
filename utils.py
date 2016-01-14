2330b6b7644ec94200d15da16538a8f308619ed9
Albert Mokrejs <albertmokrejs@gmail.com>
2016-01-14 17:50:36 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,26 @@ import hashlib
 import time
 import marshal
 import base64
+import smtplib
+
+def send_email(recipient, subject, body):
+    user = "GeoHashCache@gmail.com"
+    pwd = "NuclearPotato"
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+    except:
+        print "error"
 
 def authenticate(username,password):
     conn = sqlite3.connect("GeoHashCache.db")
