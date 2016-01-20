95a16780054339bd2efc9592d6bdeec74509bd03
Samuel Zhang <samuelzhang3@gmail.com>
2016-01-20 12:50:13 -0500

    fixes

--- geohash.py
+++ geohash.py
@@ -3,12 +3,12 @@ import urllib2
 import time
 import json
 
+#-------------------------
 #geoHash: Meant for direct access.
-#
+
 #Returns: A List containing a Latitude and Longitude.
-#
 #Params: A Latitude and Longitude, as well as a boolean indicating if the area should be reduced.
-#
+
 #It will attempt to correct inverted coordinate pairs before passing to coordGen.
 def geoHash (lat, lon, smallBool):
     if(abs(lat) > 90 and abs(lon) > 90):
@@ -19,16 +19,13 @@ def geoHash (lat, lon, smallBool):
         return coordGen(lon, lat, smallBool)
     return coordGen(lat, lon, smallBool)
 
+#------------------------
 #coordGen: Used by geoHash. 
-#
+
 #Returns: A List containing a Latitude and Longitude.
-#
 #Params: A Latitude and Longitude, as well as a boolean indicating if the area should be reduced.
-#
-#It accesses the current time, and the latest BTC:USD value call on BitAverage to make a long string of text. This text is
-#then hashed and converted to decimal values, with the front 20 digits being appended to the first 3 or 4 of the Latitude
-#and the last 20 being appended to the first 3 or 4 of the Longitutde. This pair is then truncated at 5 decimal places and
-#returned.
+
+#Accesses the current time, and the latest BTC:USD value call on BitAverage to make a long string of text. This text is then hashed and converted to decimal values, with the front 20 digits being appended to the first 3 or 4 of the Latitude and the last 20 being appended to the first 3 or 4 of the Longitutde. This pair is then truncated at 5 decimal places and returned.
 def coordGen (lat, lon, smallBool):
     #Get the date string.
     Date = time.strftime("%d-%m-%Y %H:%M:%S")
