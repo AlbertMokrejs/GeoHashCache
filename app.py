283af6abea385a6d0485e674d115ed1a7a849817
Albert Mokrejs <albertmokrejs@gmail.com>
2016-01-23 20:12:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def cache(cacheID = 0, validID = 0):
 	else:
 		if result:
 			data = utils.getCache(cacheID)
-			utils.appendProfile(session["uid"],[data["name"],data["lat"],data["lon"]])
+			utils.appendProfile(session["uid"],[[data["name"],data["lat"],data["lon"]]])
 			utils.collectCache(cacheID,data["founder"],session["user"])
 			if data["stat"] == 0:
 				data["stat"] = "Normal"
